c2aa7641ae3439cb19f38d40953dc98caded8abb
TathaagatPal <paltathaagat@gmail.com>
2025-07-11 15:01:09 -0700

    Initial commit

---
+++ src/main/java/net/tathaagat/tutorialmod/TemplateMod.java
@@ -0,0 +1,20 @@
+package net.tathaagat.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TemplateMod implements ModInitializer {
+	public static final String MOD_ID = "templatemod";
+
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
